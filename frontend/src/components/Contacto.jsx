41ad43d2d94e7b87ef475b19ba17a9c42789bff5
May <169025131+Calvinbol@users.noreply.github.com>
2024-06-22 14:05:14 +0100

    Formulario Contacto Ok (alert de envio)

--- frontend/src/components/Contacto.jsx
+++ frontend/src/components/Contacto.jsx
@@ -1,30 +1,36 @@
-import React from 'react';
-import './Contacto.css'
-
-
+import React from "react";
+import "./Contacto.css";
 
 const Contacto = () => {
-    return (
-      <>
-        <form className="formulario">
-          <h2>Contáctanos</h2>
-          <label>
-            Nombre:
-            <input type="text" name="name" className="input" />
-          </label>
-          <label>
-            Email:
-            <input type="email" name="email" className="input" />
-          </label>
-          <label>
-            Mensaje:
-            <textarea name="message" className="input textarea"></textarea>
-          </label>
-          <input type="checkbox" className='check'/>
-          <button type="submit" className='enviar'>Enviar</button>
-        </form>
-      </>
-    );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("FORMULARIO ENVIADO");
+  };
+
+  return (
+    <div className="contact-container">
+      <form className="formulario" onSubmit={handleSubmit}>
+        <h2>Contáctanos</h2>
+        <label>
+          Nombre:
+          <input type="text" name="name" className="input" required />
+        </label>
+        <label>
+          Email:
+          <input type="email" name="email" className="input" required />
+        </label>
+        <label>
+          Mensaje:
+          <textarea name="message" className="textarea" required></textarea>
+        </label>
+        <label className="check-container">
+          <input type="checkbox" className="check" required />
+          Acepto los términos y condiciones
+        </label>
+        <button type="submit" className="enviar">Enviar</button>
+      </form>
+    </div>
+  );
 };
 
 export default Contacto;
